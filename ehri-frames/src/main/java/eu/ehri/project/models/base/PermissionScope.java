f667184e238ad70dbd8c2a7e8a1a4b0a023aab08
Mike Bryant <mikesname@gmail.com>
2014-01-31 13:52:57 +0000

    Simplify PermissionScope so it's easier to implement

--- ehri-frames/src/main/java/eu/ehri/project/models/base/PermissionScope.java
+++ ehri-frames/src/main/java/eu/ehri/project/models/base/PermissionScope.java
@@ -1,11 +1,26 @@
 package eu.ehri.project.models.base;
 
 import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.frames.Adjacency;
+import com.tinkerpop.frames.modules.javahandler.JavaHandler;
+import com.tinkerpop.frames.modules.javahandler.JavaHandlerContext;
 import eu.ehri.project.definitions.Ontology;
 import eu.ehri.project.models.PermissionGrant;
+import eu.ehri.project.models.utils.JavaHandlerUtils;
 
-public interface PermissionScope extends IdentifiableEntity, AccessibleEntity {
+public interface PermissionScope extends IdentifiableEntity {
     @Adjacency(label = Ontology.PERMISSION_GRANT_HAS_SCOPE, direction = Direction.IN)
     public Iterable<PermissionGrant> getPermissionGrants();
+
+    @JavaHandler
+    public Iterable<PermissionScope> getPermissionScopes();
+
+    abstract class Impl implements JavaHandlerContext<Vertex>, PermissionScope {
+        public Iterable<PermissionScope> getPermissionScopes() {
+            return frameVertices(gremlin().as("n")
+                    .out(Ontology.HAS_PERMISSION_SCOPE)
+                    .loop("n", JavaHandlerUtils.defaultMaxLoops, JavaHandlerUtils.noopLoopFunc));
+        }
+    }
 }
