b458407ad5b10e252ae88528d6cee4a4722437b6
bencomp <ben@companjen.name>
2014-10-01 01:53:26 +0200

    Add the framework for testing persistence of annotations

---
+++ ehri-importers/src/test/java/eu/ehri/project/importers/AnnotationsTest.java
@@ -0,0 +1,51 @@
+package eu.ehri.project.importers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test whether annotations and links between descriptions
+ * persist when the descriptions are not changed or updated 
+ * on import or deleted on import.
+ * 
+ * @author bencomp <ben@companjen.name>
+ *
+ */
+public class AnnotationsTest extends AbstractImporterTest {
+
+	public AnnotationsTest() {
+		// TODO Auto-generated constructor stub
+	}
+    
+//  @Test
+    public void testAnnotationPersistenceOnNoChange() {
+        fail("Not yet implemented");
+    }
+	
+//	@Test
+	public void testAnnotationPersistenceOnUpdate() {
+		fail("Not yet implemented");
+	}
+	
+//	@Test
+	public void testAnnotationPersistenceOnDeletion() {
+		fail("Not yet implemented");
+	}
+    
+//  @Test
+    public void testLinkPersistenceOnNoChange() {
+        fail("Not yet implemented");
+    }
+	
+//	@Test
+	public void testLinkPersistenceOnUpdate() {
+		fail("Not yet implemented");
+	}
+	
+//	@Test
+	public void testLinkPersistenceOnDeletion() {
+		fail("Not yet implemented");
+	}
+
+}
